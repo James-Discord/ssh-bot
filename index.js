b3c8ff3b3f8eb352b56ffa6eaaa1a4078eeb7954
James <85806019+James-Discord@users.noreply.github.com>
2023-05-22 21:41:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -91,13 +91,7 @@ client.on('messageCreate', async (message) => {
               session.ssh.end();
               collector.stop();
             } else {
-              session.channel.write(content + '\n', 'utf-8', (err) => {
-                if (err) {
-                  dmChannel.send(`Error sending data to SSH channel: ${err.message}`);
-                  session.ssh.end();
-                  collector.stop();
-                }
-              });
+              channel.write(content + '\n');
             }
           });
 
