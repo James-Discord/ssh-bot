851b5f26a6f0e2e4814eab4e5ec6d797ff917a64
James <85806019+James-Discord@users.noreply.github.com>
2023-05-24 07:34:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -121,8 +121,8 @@ client.on('messageCreate', async (message) => {
             }
             const embed = new MessageEmbed()
               .setTitle(`SSH session for server ${sshConfig.host}`)
-              .setDescription('You are now connected via SSH. Type your commands below.')
-              .addField('Output', '```' + session.output.join('') + '```')
+              .setFooter('You are now connected via SSH. Type your commands below.')
+              .setDescription('Output', '```' + session.output.join('') + '```')
               .setColor('#00FF00');
             msg.edit({ embeds: [embed] });
           });
